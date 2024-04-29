c16ab78d02258c2b46794cdc6e70f5d7d51072df
spfamilydance <168379739+spfamilydance@users.noreply.github.com>
2024-04-29 10:57:42 -0500

    added lesson argument

--- scripts/events-list.js
+++ scripts/events-list.js
@@ -6,6 +6,15 @@ let events = [
         startTime: 1500,
         endTime: 1700,
         title: "Mother's Day Weekend Dance",
-        description: "Come celebrate a mother in your life!"
+        description: "Come celebrate a mother in your life!",
+        lesson: "Come early at 3pm for an introductory Ballroom Dance lesson."
+    },
+    {
+        date: "11 May 2024",
+        startTime: 1500,
+        endTime: 1700,
+        title: "Mother's Day Weekend Dance",
+        description: "Come celebrate a mother in your life!",
+        lesson: "Come early at 3pm for an introductory Ballroom Dance lesson."
     }
 ]
