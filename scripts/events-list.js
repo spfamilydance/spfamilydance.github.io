2653e9c84ede0182a28b7948179626137d26fd88
spfamilydance <168379739+spfamilydance@users.noreply.github.com>
2024-04-29 10:43:14 -0500

    added events list file

---
+++ scripts/events-list.js
@@ -0,0 +1,11 @@
+/*! This is the list of events */
+
+let events = [
+    {
+        date: "11 May 2024",
+        startTime: 1500,
+        endTime: 1700,
+        title: "Mother's Day Weekend Dance",
+        description: "Come celebrate a mother in your life!"
+    }
+]
